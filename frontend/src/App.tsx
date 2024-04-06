3e9b4df103698bc27bc73e9abe93b7602e7754e0
Maharshi Vashistha <maharshivashistha@Maharshis-MacBook-Air.local>
2024-04-07 03:14:14 +0530

    Medium Complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ function App() {
 	return (
 		<Router>
 			<Routes>
+				<Route path='/' element={<Signin />} />
 				<Route path='/signup' element={<Signup />} />
 				<Route path='/signin' element={<Signin />} />
 				<Route path='/blogs' element={<Blogs />} />
